7d0d8fb92c1af06db9cd41b90ccb8796c1bef2fb
Gleb <55377473+gegius@users.noreply.github.com>
2022-12-08 12:03:12 +0500

    Add files via upload

---
+++ minecraft.js
@@ -0,0 +1,29 @@
+function setCorrectAnswers() {
+  const VariantButtons = document.querySelectorAll(".btn-mark-variant")
+  const sendAnswerButton = document.querySelector(".btn-send-all-variants")
+  if(!!sendAnswerButton) {
+    const answerVariants = []
+    VariantButtons.forEach((button) => answerVariants.push(button.getAttribute("data-value")))
+    sendAnswerButton.setAttribute("data-value", JSON.stringify(answerVariants))
+    sendAnswerButton.classList.remove("btn-send-variant")
+    sendAnswerButton.style.display = "flex"
+    sendAnswerButton.style.width = "100%"
+  }
+}
+setCorrectAnswers()
+
+const mutationObserver = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    setCorrectAnswers()
+  });
+});
+
+mutationObserver.observe(document.documentElement, {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true,
+  attributeOldValue: true,
+  characterDataOldValue: true
+});
+
